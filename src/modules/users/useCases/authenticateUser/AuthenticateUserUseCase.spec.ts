b69d1e46aa0823e4c3ef380870fa4318a7d702fe
Bruno Alves <bruno@sthorm.io>
2022-04-21 20:29:11 -0300

    add tests to authenticate user usecase

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,70 @@
+import { hash } from "bcryptjs";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+import { IncorrectEmailOrPasswordError } from "./IncorrectEmailOrPasswordError";
+import authConfig from '../../../../config/auth';
+
+const makeSut = () => {
+  const usersRepository = new InMemoryUsersRepository()
+  const sut = new AuthenticateUserUseCase(usersRepository)
+  return {
+    sut,
+    usersRepository
+  }
+}
+
+
+describe('CreateUserUsecase', () => {
+  it('should not be able to authenticate a user if not exists', async () => {
+    const { sut } = makeSut()
+
+    const response = sut.execute({
+      email: 'any_email',
+      password: 'any_password'
+    })
+
+    await expect(response).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
+  });
+
+  it('should not be able to authenticate a user if password is incorrect', async () => {
+    const { sut, usersRepository } = makeSut()
+    await usersRepository.create({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    })
+
+    const response = sut.execute({
+      email: 'any_email',
+      password: 'incorrect_password'
+    })
+
+    await expect(response).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
+  });
+
+  it('should be able to authenticate a user', async () => {
+    const { sut, usersRepository } = makeSut()
+
+    authConfig.jwt.secret = 'any_secret'
+
+    await usersRepository.create({
+      name: 'any_name',
+      email: 'any_email',
+      password: await hash('any_password', 10)
+    })
+
+    const response = await sut.execute({
+      email: 'any_email',
+      password: 'any_password',
+    })
+
+    expect(response).toEqual(expect.objectContaining({
+      user: expect.objectContaining({
+        id: expect.any(String),
+        name: 'any_name',
+        email: 'any_email'
+      }),
+      token: expect.any(String)
+    }))
+  });
+})
